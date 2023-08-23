b101fdd8ae7332935ea9f52ef2716cba01526cbe
zhiqin14 <2184351932@qq.com>
2023-08-23 21:52:28 +0800

    fix

--- b64code2img.py
+++ b64code2img.py
@@ -1,6 +1,8 @@
+import base64
+
 class B64code2Img:
     def __init__(self, b64code):
-        imgdata = b64code
+        imgdata = base64.b64decode(b64code)
         img = open('qr.png', 'wb')
         img.write(imgdata)
         img.close()
